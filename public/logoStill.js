d702c802e4caaa36c623f4596bac25bc58727d65
Angus Wong <acfwong8@gmail.com>
2016-02-27 21:34:28 +0100

    colour changes

--- public/logoStill.js
+++ public/logoStill.js
@@ -135,8 +135,14 @@ $(document).on("scroll",function(){
     var lock = 100;
     var canvasHeight = startHeight - top;
     var lockedCanvas = startHeight - lock;
+    var red = 0,
+        green = 51,
+        blue = 102,
+        opacity = 1 - 0.6;
+    var shiftOpacity = 1 - (top * opacity/lock);
     if (top <= lock){
-        $("header").css({'top':0});
+        $("header").css({'top':0, 'background': 'rgba('+red+','+green+','+blue+',' + shiftOpacity + ')'});
+        $("body").css({'top':0, 'background': 'rgba('+red+','+green+','+blue+',' + shiftOpacity + ')'});
         $(".toolbar").css({'left': Math.min(3 + 17 * (top/100)) + '%'});
         $("canvas").css({'height': canvasHeight,
                          'width': canvasHeight * 5.333333,
